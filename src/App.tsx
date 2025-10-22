d1d668631e0627cb005fbf36bb6f690dfcc32635
liskasYR <168289457+liskasYR@users.noreply.github.com>
2025-10-22 22:02:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import AiModelsPage from "./pages/AiModelsPage";
 import FormsPage from "./pages/FormsPage";
+import Wiki from "./pages/Wiki";
+import GamesPage from "./pages/GamesPage";
+import DocsPage from "./pages/DocsPage";
 import MaintenancePage from "./pages/MaintenancePage";
+import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MaintenancePage />} />
-          <Route path="/forms" element={<FormsPage />} /> 
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/models" element={<AiModelsPage />} />
+        <Route path="/forms" element={<FormsPage />} />
+        <Route path="/wiki" element={<Wiki />} />
+        <Route path="/games" element={<GamesPage />} />
+        <Route path="/docs" element={<DocsPage />} />
+        <Route path="/maintenance" element={<MaintenancePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
