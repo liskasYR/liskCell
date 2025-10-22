b3cdebe493447d368b15903577665db45ee302c2
liskasYR <168289457+liskasYR@users.noreply.github.com>
2025-10-22 18:43:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<MaintenancePage />} />
           <Route path="/forms" element={<FormsPage />} /> 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
